8e3c35f35ea35eb0247c27eb864de9796e9230fc
Jaren Glenn <jarenglenn@gmail.com>
2021-10-09 15:43:49 -0600

    added nav buttons and drawer

--- client/hooks.ts
+++ client/hooks.ts
@@ -1,6 +1,7 @@
 import { makeUseAxios, Options } from 'axios-hooks';
 import { Method } from 'axios';
 import api from './api';
+import { RefObject, useEffect } from 'react';
 
 const useAxios = makeUseAxios({
 	axios: api,
@@ -24,3 +25,33 @@ export const usePost = makeLazy('POST');
 export const usePut = makeLazy('PUT');
 export const useDelete = makeLazy('DELETE');
 export const useLazyGet = makeLazy('GET');
+
+type AnyEvent = MouseEvent | TouchEvent;
+
+export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+	ref: RefObject<T>,
+	handler: (event: AnyEvent) => void
+): void {
+	useEffect(() => {
+		const listener = (event: AnyEvent) => {
+			const el = ref?.current;
+
+			// Do nothing if clicking ref's element or descendent elements
+			if (!el || el.contains(event.target as Node)) {
+				return;
+			}
+
+			handler(event);
+		};
+
+		document.addEventListener('mousedown', listener);
+		document.addEventListener('touchstart', listener);
+
+		return () => {
+			document.removeEventListener('mousedown', listener);
+			document.removeEventListener('touchstart', listener);
+		};
+
+		// Reload only if ref or handler changes
+	}, [ref, handler]);
+}

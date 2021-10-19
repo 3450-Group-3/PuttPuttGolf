2b0a76e9d8d1bb8625b5697a40a66561ce7a03e1
Adam Alder <atariq11700@gmail.com>
2021-10-19 11:43:22 -0600

    added styled components

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,9 +1,20 @@
 import { useState } from 'react';
 import { usePost } from '../hooks';
 import { FormError } from '../types';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Input from '../common/Input';
+import styled from 'styled-components';
+import { Button, defaultTheme } from '../common/styles';
+import { sleep } from '../common/utils';
 
 type LoginSuccess = { access_token: string };
+
+const Div = styled.div`
+	text-align: center;
+	a {
+		color: ${defaultTheme.accent};
+	}
+`;
 
 export default function Login() {
 	const [state, setState] = useState({
@@ -16,6 +27,12 @@ export default function Login() {
 		LoginSuccess,
 		FormError<keyof typeof state>
 	>('/auth/token');
+
+	
+	if (data){
+		sleep(3000);
+		window.location.href = "/play";
+	}
 
 	const handleLogin = () => {
 		const form = new FormData();
@@ -30,29 +47,26 @@ export default function Login() {
 		});
 	};
 
-	//todo: use styled components from common
 	return (
-		<div>
-			<img src="/static/images/logo.png" alt="logo png" width="400" height="400"></img>
+		<Div>
+			<img src="/static/images/logo.png" alt="logo png" width="400em" height="300em"></img>
 			<h3>Please sign into your account</h3>
 			{loading && <div>Logging in...</div>}
 			{error && <div>Login failed :(</div>}
 			{data && <div>Login Success!</div>}
-			<input
-				value={username}
-				onChange={(e) => setState({ username: e.target.value, password })}
+			<Input
+				// title="Username"
 				placeholder="Username"
+				onChange={(e) => setState({ username: e.target.value, password })}
 			/>
-			<br />
-			<input
-				value={password}
-				onChange={(e) => setState({ password: e.target.value, username })}
+			<Input
+				// title="Password"
 				placeholder="Password"
 				type="password"
+				onChange={(e) => setState({ password: e.target.value, username })}
 			/>
-			<br />
 			<h5>Click <Link to="/signup">here</Link> to sign up for an account</h5>
-			<button onClick={() => handleLogin()}>Login</button>
-		</div>
+			<Button onClick={() => handleLogin()}>Login</Button>
+		</Div>
 	);
 }

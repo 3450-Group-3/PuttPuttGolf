ee1962eb8f5af646c56d7e04847b39a69f59a158
Sean Collings <seanrcollings@gmail.com>
2021-10-19 21:43:27 -0600

    added light theme

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -11,9 +11,11 @@ import AccountManagement from './AccountManagement';
 import Home from './Home';
 import Login from './Login';
 import SignUp from './SignUp';
-import { defaultTheme, GlobalStyle } from '../common/styles';
+import { themes, GlobalStyle, Button } from '../common/styles';
 import NotFound from './NotFound';
 import LayoutManager from './LayoutManager';
+import { useState } from 'react';
+import Themer from './Themer';
 
 const ProtectedRoute = (props: RouteProps) => {
 	if (localStorage.getItem('token')) {
@@ -23,8 +25,12 @@ const ProtectedRoute = (props: RouteProps) => {
 };
 
 export default function App() {
+	const [theme, setTheme] = useState<string>(
+		localStorage.getItem('theme') || 'darkMode'
+	);
+
 	return (
-		<ThemeProvider theme={defaultTheme}>
+		<ThemeProvider theme={themes[theme]}>
 			<GlobalStyle />
 			<Router>
 				<LayoutManager>
@@ -32,6 +38,12 @@ export default function App() {
 						<Route path="/" exact component={Home} />
 						<Route path="/signup" exact component={SignUp} />
 						<Route path="/login" component={Login} />
+						<Route
+							path="/theme"
+							render={(props) => (
+								<Themer {...props} theme={theme} setTheme={setTheme} />
+							)}
+						></Route>
 						<ProtectedRoute path="/me" component={AccountManagement} />
 						<Route path="*" component={NotFound} />
 					</Switch>

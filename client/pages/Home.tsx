9fd3db262a8ec4b83addbf9f0831544ac2d0bb24
Jonathan Gates <jonathangates33@gmail.com>
2021-10-26 18:48:51 -0600

    homepage

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,30 +1,37 @@
-import { Link } from 'react-router-dom';
 import { useUser } from '../hooks';
+import styled from 'styled-components';
+import { ButtonLink, CenterContent } from '../styles';
+
+
+export const Description = styled.p`
+	font-size: 25px;
+	padding: 1rem;
+	text-align: center;
+`;
 
 export default function Home() {
 	const { user, setUser } = useUser();
 
 	return (
-		<div>
-			<img src="static/images/logo.png" alt="logo"/>
-			<br />
+		<CenterContent>
+			<img src="static/images/logo.png"
+			alt="logo"
+			width="400em"
+			height="300em"/>
 
-			<p>Welcome to the PuttPuttGolf and Drink App</p>
-
-			<Link to="/me">Account Info</Link>
-			<br />
+			<Description>Welcome to the PuttPuttGolf and Drink App. In this app you are able to join a tournment, add your Putt score, and order drinks (Age 21+).</Description>
 
 			{!user.loggedIn ? (
 				<>
-					<Link to="/signup">Sign Up</Link>
+					<ButtonLink to="/signup">Sign Up</ButtonLink>
 					<br />
-					<Link to="/login">Login</Link>
+					<ButtonLink to="/login">Login</ButtonLink>
 				</>
 			) : (
 				<a onClick={() => localStorage.removeItem('token')} href="">
 					Logout
 				</a>
 			)}
-		</div>
+		</CenterContent>
 	);
 }

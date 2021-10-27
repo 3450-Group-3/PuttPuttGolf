298afb1f53c9fcd95c8d6364aa2b5d546b8b333b
Adam Alder <atariq11700@gmail.com>
2021-10-27 13:08:02 -0600

    added pair class

--- app/utils.py
+++ app/utils.py
@@ -6,3 +6,9 @@ def to_camel(string: str, upper_camel_case=False):
             for idx, word in enumerate(string.split("_"))
         ]
     )
+
+
+class Pair():
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second

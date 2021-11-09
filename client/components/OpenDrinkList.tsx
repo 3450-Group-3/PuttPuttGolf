1cec10dbda8429a42e629a621b7f0a035da11365
Adam Alder <atariq11700@gmail.com>
2021-11-09 13:47:43 -0700

    added rendering of open drink list

--- client/components/OpenDrinkList.tsx
+++ client/components/OpenDrinkList.tsx
@@ -1,5 +1,59 @@
-export default function OpenDrinkList() {
+import styled from "styled-components"
+import { useGet } from "../hooks"
+import { DrinkOrderData, DrinkOrderState } from "../pages/DrinkOrderFufillment"
+import { DetailFormError } from "../types"
+import { DrinkData } from "../types"
+
+
+const DrinkContainer = styled.div`
+    border: 1px solid black
+`
+
+
+
+export default function OpenDrinkList() { //todo move fetching of data to drinkorderfullfillment component
+
+    function getDrinkData(id: number): DrinkData | undefined {
+        const {data, loading, error} = useGet<DrinkData, DetailFormError>("/drinks/" + id)
+        return data;
+    }
+
+    const {data, loading, error} = useGet<DrinkOrderData[], DetailFormError>("/orders/state/" + DrinkOrderState.OPEN)
+    const status = ["OPEN", "INPROGRESS", "ENROUTE", "DELIVERED"]
+
+    console.log(data)
+
+    if (!data || data.length == 0){
+        return (
+            <h2>No open orders at this time</h2>
+        )
+    }
+
     return (
-        <p>i am open drink list</p>
+        <div>
+            {data.map((drinkOrder) => {
+                return (
+                    <div key={drinkOrder.id}>
+                        <p>Order id {drinkOrder.id}</p>
+                        <p>customer id {drinkOrder.customer_id}</p>
+                        <p>status {status[drinkOrder.order_status]}</p>
+                        <p>time ordered {drinkOrder.time_ordered}</p>
+                        <p>Drinks</p>
+                        {drinkOrder.drinks.map((drink) => {
+                            const drinkData = getDrinkData(drink.drinkId)
+                            if (drinkData){
+                                return (
+                                    <DrinkContainer key={drinkData.id}>
+                                        <p>Drink Name {drinkData.name}</p>
+                                        <p>AMount {drink.drinkQty}</p>
+                                    </DrinkContainer>
+                                )
+                            }
+                        })}
+                        <p>DrinkMeister assigned {drinkOrder.drink_meister_id}</p>
+                    </div>
+                )
+            })}
+        </div>
     )
 }

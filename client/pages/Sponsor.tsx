666b872934b4c1e3f16a5959fab372720e3a6e55
Jonathan Gates <jonathangates33@gmail.com>
2021-11-09 20:18:21 -0700

    added some db fuctionality, database needs to be updated to accept percentages for 1st 2nd 3rd places

--- client/pages/Sponsor.tsx
+++ client/pages/Sponsor.tsx
@@ -1,10 +1,13 @@
-import { useUser } from '../hooks';
+import { useGet, useUser } from '../hooks';
 import styled from 'styled-components';
 import { Button, CenterContent } from '../styles';
 import Title from '../components/Title';
 import { TournamentData } from '../types';
 import { usePut } from '../hooks';
 import TextInput from '../components/TextInput';
+import { useParams } from 'react-router';
+import { useForm } from 'react-hook-form';
+
 
 export const Description = styled.p`
 	font-size: 25px;
@@ -16,19 +19,40 @@ export const FlexBox = styled.div`
     display: flex;
 `;
 
+interface Inputs{
+    first: number; 
+    second: number; 
+    third: number;
+    total: number;
+    banner: string;
+}
+
 export default function Sponsor() {
     const { user, setUser } = useUser();
-    return (
-    <CenterContent>
-        <Title>Sponsor</Title>
-
-        {/* Tournament Details  Date - Time - Number of holes (From EditTournament.tsx)*/}
-        {/* const {
+    const { id = null } = useParams<{ id: string }>();
+	const {
 		data: tournament,
 		loading,
 		error,
 		refetch,
-	} = useGet<TournamentData>(`/tournaments/${id}`); */}
+	} = useGet<TournamentData>(`/tournaments/${id}`);
+    
+
+    const [
+		{ data: updateData, loading: updateLoading, error: updateError },
+		update,
+	] = usePut<TournamentData>(`/tournaments/${id}`);
+
+    const {
+		register,
+		formState: { errors },
+		control,
+		handleSubmit,
+	} = useForm<Inputs>({defaultValues: {first:50, second:30, third:20, total:3, banner:""}});
+    
+    return (
+    <CenterContent>
+        <Title>Sponsor</Title>
 
         {/* Sponser Money Amount */}
 
@@ -36,13 +60,19 @@ export default function Sponsor() {
 
         {/* Prize portion of sponsered amount */}
         <Description>Winnings Pool</Description>
+        <form onSubmit={handleSubmit((data) => update({data: {...tournament, balance: data.total, advertisingBanner: data.banner}}))}>
         <TextInput
-				placeholder="Tournament Prize"
-                type="number"
-                min="0"
-                max={user.balance}
-                defaultValue="3"
-			/>
+            {...register('total', {
+                required: 'Must have sponsor money',
+                min: 3,
+                valueAsNumber: true,
+                validate: (v) => !isNaN(v),
+            })}
+            error={errors.total?.message}
+            placeholder="Tournament Prize"
+            type="number"
+            max={user.balance}
+        />
 
         {/* | 1st % | 2nd % | 3rd % |*/}
         <Description>Percentage of Winnings</Description>
@@ -50,32 +80,42 @@ export default function Sponsor() {
         <FlexBox>
         <br />
             1st:<TextInput
+                {...register('first', {
+                    required: 'Must have number',
+                    min: 33,
+                    valueAsNumber: true,
+                    validate: (v) => {return !isNaN(v)},
+                })}
                 id="1stPercentage"
 				placeholder="1st Place Percentage"
                 type="number"
-                min="33"
                 max="98"
-                defaultValue="50"
                 />
                 <br />
 
             2nd:<TextInput
+                {...register('second', {
+                    required: 'Must have number',
+                    min: 1,
+                    valueAsNumber: true,
+                    validate: (v) => !isNaN(v),
+                })}
                 id="2ndPercentage"
                 placeholder="2nd Place Percentage"
                 type="number"
-                min="1"
-                max="50"
-                defaultValue="30"
                 />
                 <br />
 
             3rd:<TextInput
+                {...register('third', {
+                    required: 'Must have number',
+                    min: 1,
+                    valueAsNumber: true,
+                    validate: (v) => !isNaN(v),
+                })}
                 id="3rdPercentage"
                 placeholder="3rd Place Percentage"
                 type="number"
-                min="1"
-                max="50"
-                defaultValue="20"
                 />
                 <br />
         </FlexBox>
@@ -85,7 +125,8 @@ export default function Sponsor() {
         <TextInput placeholder="Advertising banner URL"/>
 
         {/* Confirm and sponsor */}
-        <Button>Confirm and Sponsor</Button>
+        <Button type="submit">Confirm and Sponsor</Button>
+        </form>
 
     </CenterContent>
     );

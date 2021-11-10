bcd14bf9493be2cded7070a15cdbd90714e93a75
Jonathan Gates <jonathangates33@gmail.com>
2021-11-09 17:40:59 -0700

    Sponsor Tournament Added to routes

--- client/routes.tsx
+++ client/routes.tsx
@@ -10,6 +10,7 @@ import UserManagement from './pages/UserManagement';
 import Tournaments from './pages/Tournaments';
 import CreateTournament from './pages/CreateTournament';
 import EditTournament from './pages/EditTournament';
+import Sponsor from './pages/Sponsor';
 
 /**  User must be logged in */
 const AuthRoute = (props: RouteProps) => {
@@ -62,6 +63,7 @@ export default function Routes() {
 			<Route path="/" exact component={Home} />
 			<Route path="/signup" exact component={SignUp} />
 			<Route path="/login" exact component={Login} />
+			<AuthRoute path="/tournaments/:id/sponsor" component={Sponsor} />
 			<AuthRoute path="/users/:id" component={AccountManagement} />
 			<AuthRoute path="/me" exact component={AccountManagement} />
 			<PermissionRoute

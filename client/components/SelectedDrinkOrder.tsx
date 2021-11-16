896ab58475b2c9115c06156a260f96dec8be5bb2
Adam Alder <atariq11700@gmail.com>
2021-11-16 12:53:31 -0700

    added order data fields

--- client/components/SelectedDrinkOrder.tsx
+++ client/components/SelectedDrinkOrder.tsx
@@ -1,5 +1,29 @@
+import { useGet, useUser } from "../hooks"
+import { DrinkOrderData } from "../pages/DrinkOrderFufillment";
+import { DetailFormError } from "../types";
+
 export default function SelectedDrinkOrder() {
+
+    const {user} = useUser();
+    const {data, loading, error} = useGet<DrinkOrderData[], DetailFormError>("/orders/user/" + user.id)
+
     return (
-        <p>I am a specific order</p>
+        <div>
+            {data && <div>
+                <p>Drink Order Id: {data.at(0)?.id}</p>
+                <p>Customer Name: {data.at(0)?.customerName}</p>
+                <p>Order Status: {data.at(0)?.orderStatus}</p>
+                <p>Time Ordered" {data.at(0)?.timeOrdered}</p>
+                <p>Order Total Price: {data.at(0)?.totalPrice}</p>
+                <p>Drinks
+                    <div style={{marginLeft: "2em"}}>
+                        {data.at(0)?.drinks.map((drink) => {
+                                return 
+                            })}
+                    </div>
+                </p>
+            </div>}
+            {loading && <p>Loading order data...</p>}
+        </div>
     )
-}
+} 
